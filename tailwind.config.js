862f239c232af52a95afe167f7a3c5d3f7183015
Damodar Lohani <dlohani48@gmail.com>
2021-09-03 12:19:25 +0545

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-	purge: ['./index.html', './src/**/*.{svelte,js,ts}'],
+	purge: {
+		enable : true,
+		content: ['./index.html', './src/**/*.{svelte,js,ts}'],
+		options : {
+		safelist: [/^w-/, /^bg-[a-z]*-*/]
+		},
+	  },
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
